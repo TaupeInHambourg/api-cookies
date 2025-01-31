4b0f08ed9cdafaa61057f794062783d56f6bd01e
Audrey Norvez <audrey.norvez@gmail.com>
2025-01-31 01:15:59 +0100

    debug render

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,10 @@
 var express = require('express');
 var router = express.Router();
 
+router.get('/', (req, res) => {
+    res.status(200).json({ message: "API is running!" });
+});
+
 router.get('/api', function(req, res, next){
     res.status(200).json({message:"hello"});
 })
